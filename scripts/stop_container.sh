60959249294338938dc67360198725172e782a67
bindu <bindupriya2508@gmail.com>
2026-03-16 12:39:15 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop and remove running containers (if any)
+containerId=$(docker ps -q)
+
+if [ -n "$containerId" ]; then
+  docker rm -f $containerId
+else
+  echo "No running containers found"
+fi
